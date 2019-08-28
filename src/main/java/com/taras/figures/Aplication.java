a1209676abc5b17e3f423d63ebc26803de54186e
Taras Khalak <tarasulo@gmail.com>
2019-08-28 13:25:19 +0300

    Array

--- src/main/java/com/taras/figures/Aplication.java
+++ src/main/java/com/taras/figures/Aplication.java
@@ -13,26 +13,30 @@ import static java.lang.System.*;
 
 public class Aplication {
     public static void main(String[] args) {
-        Figures2D square = new Square(5.0);
-        Figures2D rectangle = new Rectangle(2.2, 4.1);
-        Figures2D circle = new Circle(3.0);
-        Figures3D cube = new Cube(4.0);
-        Figures3D ball = new Ball(2.0);
-        Figures3D parallelepiped = new Parallelepiped(2.0, 1.5, 3);
-        Figures2D ball2 = new Ball(2.0);
-        Figures2D cube2 = new Cube(5.0);
-        Figures2D parallelepiped2 = new Parallelepiped(5.0, 1.0, 3);
-        out.println("The square of square: " + square.getSquare());
-        out.println("The perimeter of square: " + square.getPerimeter());
-        out.println("The square of rectangle " + rectangle.getSquare());
-        out.println("The perimeter of rectangle: " + rectangle.getPerimeter());
-        out.println("The square of circle: " +circle.getSquare());
-        out.println("The length of the circle: " + circle.getPerimeter());
-        out.println("The capacity of the cube: " + cube.getCapacity());
-        out.println("The capacity of the ball: " + ball.getCapacity());
-        out.println("The capacity of the parallelepiped: " + parallelepiped.getCapacity());
-        out.println("The length of the ball: " + ball2.getPerimeter());
-        out.println("The square of cube: " +cube2.getSquare());
-        out.println("The square of parallelepiped: " +parallelepiped2.getSquare());
+        Figures2D square = new Square(Math.random());
+        Figures2D rectangle = new Rectangle(Math.random(), Math.random());
+        Figures2D circle = new Circle(Math.random());
+        Figures2D[] figures2D = new Figures2D[] {
+                square,
+                rectangle,
+                circle };
+        for (int i = 0; i< figures2D.length; i++) {
+            figures2D[i].name();
+            out.println("The square of figure: " + figures2D[i].getSquare());
+            out.println("The perimeter of figure: " + figures2D[i].getPerimeter());
+        }
+
+        Figures3D cube = new Cube(Math.random());
+        Figures3D ball = new Ball(Math.random());
+        Figures3D parallelepiped = new Parallelepiped(Math.random(), Math.random(), Math.random());
+        Figures3D[] figures3D = new Figures3D[] {
+                cube,
+                ball,
+                parallelepiped };
+
+        for (int i = 0; i< figures2D.length; i++) {
+            figures3D[i].name();
+            out.println("The capacity of figure: " + figures3D[i].getCapacity());
+        }
     }
 }
